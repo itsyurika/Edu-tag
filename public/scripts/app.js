d8d59bc655b7a964f43258f8e417609d70963663
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-09 17:14:08 -0400

    Fixes an issue with the rows caused by --bs-gutter-x being set to 1.5rem.  Solution is to add class g-0 to div containing the row class to remove the gutter.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,7 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 0) {
         document.getElementById('navbar_top').classList.add('fixed-top');
         // add padding top to show content behind navbar
         navbar_height = document.querySelector('.navbar').offsetHeight;
